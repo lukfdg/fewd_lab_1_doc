1e19f7a9e925ea3464f87a46f5b50e1f8f93700b
Lucas_Souza <lukfdg@gmail.com>
2020-02-02 15:14:17 +0000

    ‘commit’

---
+++ app.js
@@ -0,0 +1,23 @@
+var firstName = 'Lucas'; // Question 3 - Declare a variable ​firstName​ and initialize it with the value of ​your name​.
+var favouriteColor = 'Green'; // Question 4 - Declare a variable​ favouriteColor​ and initialize it with a ​string value​.
+var favouriteFood = 'Pizza'; // Question 5 - Declare a variable​ favouriteFood​ and assign it with a value.
+var city = 'Dublin'; // Question 5 - Declare a secondvariable ​city​ and assign it a value.
+ 
+// Question 8 and 9 - Function necessary to print the variables which were defined before in questions 3,4 and 5.
+console.log(firstName);
+console.log(favouriteColor);
+console.log(favouriteFood, city);
+
+//Question 10 - Variables create in a specific sequence / order to be showed.
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!'
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+//Question 11 - Variables displayed in a specific sequence / order.
+console.log(phrase, one, two, three, four, five, six, seven, endingPhrase);
